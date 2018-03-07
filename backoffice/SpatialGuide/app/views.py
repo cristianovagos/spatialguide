160dbad6e095ab6bd2d6080309a0e82dffc504a2
Nuno <nuno.abarbosa@hotmail.com>
2018-03-07 10:26:08 +0000

    Name Change

---
+++ backoffice/SpatialGuide/app/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.http import HttpResponse
+from datetime import datetime
+
+from app import BD_Connector
+
+
+# Create your views here.
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    tparams = {
+        'title':'Home Page',
+        'year':datetime.now().year,
+    }
+    return render(request, 'index.html', tparams)
+
+def tables(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+
+    return render(request, 'tables.html', {})
+
+def charts(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+
+    return render(request, 'charts.html', {})
+
+def cards(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+
+    return render(request, 'cards.html', {})
+
+def register(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+
+    return render(request, 'register.html', {})
+
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    tparams = {
+        'title':'Contact',
+        'message':'Your contact page.',
+        'year':datetime.now().year,
+    }
+    return render(request, 'contact.html', tparams)
+
+
+def about(request):
+    assert isinstance(request, HttpRequest)
+    tparams = {
+        'title': 'About',
+        'message': 'Your application description page.',
+        'year': datetime.now().year,
+    }
+    return render(
+        request,
+        'about.html',
+        {
+            'title': 'About',
+            'message': 'Your application description page.',
+            'year': datetime.now().year,
+        }
+    )
+
