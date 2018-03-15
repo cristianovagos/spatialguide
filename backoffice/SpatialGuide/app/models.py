5ffdfb37c8049698198ba8c31b9aeb3684731ab0
Nuno <nuno.abarbosa@hotmail.com>
2018-03-15 23:44:04 +0000

    Adding Routes

--- backoffice/SpatialGuide/app/models.py
+++ backoffice/SpatialGuide/app/models.py
@@ -1,23 +1,38 @@
 from django.db import models
+from django.forms import ModelForm
+from django import forms
 
 class Route(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
+    Name = models.CharField(max_length=50)
+    Description = models.CharField(max_length=100)
 
     def __str__(self):
-        return "{'name': %s, 'description': %s}" % (self.name,self.description)
+        return "{'name': %s, 'description': %s}" % (self.Name,self.Description)
+
+
 
 class Point(models.Model):
-    name = models.CharField(max_length=50)
-    latitude = models.FloatField()
-    longitude = models.FloatField()
+    Name = models.CharField(max_length=50)
+    Latitude = models.FloatField()
+    Longitude = models.FloatField()
 
     def __str__(self):
-        return "Name: %s ( %s, %s )" %(self.name,self.x,self.y)
+        return "Name: %s ( %s, %s )" %(self.Name,self.Latitude,self.Longitude)
 
 class Route_contains_Point(models.Model):
-    route = models.ForeignKey(Route)
-    point = models.ForeignKey(Point)
+    Route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    Point = models.ForeignKey(Point, on_delete=models.CASCADE)
 
     def __str__(self):
         return "Route '%s' contains point '%s'" %(self.route.name,self.point.name)
+
+
+
+
+class RouteForm(ModelForm):
+    Name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Name'}))
+    Description = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Description'}))
+
+    class Meta:
+        model = Route
+        fields = '__all__'
